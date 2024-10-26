91433205dc91b67c8308f1cb704e74e91c645da6
Ariane-PS <126026583+Ariane-PS@users.noreply.github.com>
2024-10-26 16:32:37 -0300

    Update conexao1.php

--- conexao1.php
+++ conexao1.php
@@ -1,17 +1,17 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "login";
-$port = 3306;
+$host = "mysql995.umbler.com";
+$user = "loginusuarios";
+$pass = "ocultado";
+$dbname = "loginusuarios";
+//$port = 41890;
 
 try{
     //Conexão com a porta
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+    //$conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
 
     //Conexão sem a porta
-    //$conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
     //echo "Conexão com banco de dados realizado com sucesso!";
 }catch(PDOException $err){
     //echo "Erro: Conexão com banco de dados não realizado com sucesso. Erro gerado " . $err->getMessage();
