dae0a268624aee2b40858458377b2206a22d39cc
Ariane-PS <126026583+Ariane-PS@users.noreply.github.com>
2024-10-26 15:36:32 -0300

    Update conexao1.php

--- conexao1.php
+++ conexao1.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "d6vscs19jtah8iwb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user = "sw6p92x30ich4qr2";
-$pass = "zppl62ubyafegqsb";
-$dbname = "tebq3vvvk4c5gfts";
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "login";
 $port = 3306;
 
 try{
